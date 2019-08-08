fbf6d563ea6b9704bf081c4a4fcc2ce74df65889
gaojx <gaojxcs@gmail.com>
2019-08-08 16:13:25 +0800

    push command

---
+++ push_image.sh
@@ -0,0 +1,14 @@
+set -e
+set -x
+
+image_name="bioner"
+image_version=0.1
+image_name=$image_name:$image_version
+
+docker build \
+    --build-arg AK \
+    --build-arg SK \
+    -t $image_name .
+
+docker tag $image_name $HW_IMAGE_PREFIX/$image_name
+docker push $HW_IMAGE_PREFIX/$image_name
